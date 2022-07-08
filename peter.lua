8e55d918a9e62b7f18003fb2a0f9cffc21e489bb
LD2065 <89920318+LD0265@users.noreply.github.com>
2022-07-08 15:01:12 -0400

    Update peter.lua

--- peter.lua
+++ peter.lua
@@ -1,3 +1,5 @@
+--[ElEmDee]
+
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
 
 local w = library:CreateWindow("ElEmDee pete farm") -- Creates the window
