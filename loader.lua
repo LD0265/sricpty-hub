f51a2e699d88faf2e85bd0a7110acdb22e0b9ac3
LD2065 <89920318+LD0265@users.noreply.github.com>
2022-07-08 15:54:38 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,27 @@
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+
+local notif = Notification.new("info", "Checking Executor", "Checking Executor")
+wait(3)
+notif:delete()
+wait(1)
+if syn then
+local notif = Notification.new("info", "Executor Detected", "Synapse Detected")
+    wait(2)
+notif:delete()
+    wait(1)
+local notif = Notification.new("success", "Success", "Executing Script")
+    wait(1)
+    notif:delete()
+else
+    local notif = Notification.new("info", "Executor Detected", "Syanpse Only")
+end
+
+local chosenGame = ({
+    [9571238478] = "peter.lua",
+    [9203864304] = "floppa.lua"
+})[game.PlaceId]
+
+if chosenGame then
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/LD0265/sricpty-hub/main/" .. chosenGame))()
+  else
+  end
