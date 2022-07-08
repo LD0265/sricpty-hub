a6b78b855eef170f6b3f54f45ca543a6a7fa8a6c
LD2065 <89920318+LD0265@users.noreply.github.com>
2022-07-08 16:23:00 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,20 +1,12 @@
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
 
-local notif = Notification.new("info", "Checking Executor", "Checking Executor")
-wait(3)
-notif:delete()
-wait(1)
-if syn then
-local notif = Notification.new("info", "Executor Detected", "Synapse Detected")
-    wait(2)
-notif:delete()
-    wait(1)
 local notif = Notification.new("success", "Success", "Executing Script")
     wait(1)
     notif:delete()
 else
     local notif = Notification.new("info", "Executor Detected", "Syanpse Only")
 end
+print(game.PlaceId)
 
 local chosenGame = ({
     [9660733037] = "peter.lua",
