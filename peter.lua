ff231b81434b02807ba445cc5a0497fc78a13fca
LD2065 <89920318+LD0265@users.noreply.github.com>
2022-07-08 17:56:21 -0400

    Update peter.lua

--- peter.lua
+++ peter.lua
@@ -4,9 +4,9 @@ local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloo
 
 local w = library:CreateWindow("ElEmDee Hub") -- Creates the window
 
-local b = w:CreateFolder("evyn fat")
+local b = w:CreateFolder("Main")
 
-local c = w:CreateFolder("misc")-- Creates the folder(U will put here your buttons,etc)
+local c = w:CreateFolder("Misc")-- Creates the folder(U will put here your buttons,etc)
 
 b:Label("Autofarm",{
     TextSize = 25; -- Self Explaining
