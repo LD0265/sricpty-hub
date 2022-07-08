6c47b2e61e6ddd237d3798db73101d57aa98e216
LD2065 <89920318+LD0265@users.noreply.github.com>
2022-07-08 15:41:28 -0400

    Update floppa.lua

--- floppa.lua
+++ floppa.lua
@@ -1,5 +1,31 @@
---raise a floppa
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
 
-local floppa = game:GetService("Workspace").Floppa.ClickDetector
+local w = library:CreateWindow("Floppa Farm") -- Creates the window
 
-fireclickdetector(floppa)
+local b = w:CreateFolder("Sheesh") -- Creates the folder(U will put here your buttons,etc)
+
+b:Label("n wor",{
+    TextSize = 25; -- Self Explaining
+    TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
+    BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
+}) 
+
+b:Toggle("Autoclick floppa",function(bool)
+   getgenv().startaction = bool
+   print('Action is: ', bool)
+   if bool then
+       doaction()
+   end
+end)
+
+b:DestroyGui()
+
+function doaction()
+    spawn(function()
+        while getgenv().startaction == true  do 
+           local floppa = game:GetService("Workspace").Floppa.ClickDetector
+            fireclickdetector(floppa)
+            wait()
+        end
+    end)
+end
