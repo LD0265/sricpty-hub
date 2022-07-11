9bc7b7f111d1c2bac54d85124085c08f68a0eb61
LD2065 <89920318+LD0265@users.noreply.github.com>
2022-07-11 19:11:50 -0400

    Update floppa2op.lua

--- floppa2op.lua
+++ floppa2op.lua
@@ -37,6 +37,14 @@ b:Toggle("Autoclick Floppa",function(bool)
    end
 end)
 
+b:Toggle("AutoClick Babies",function(bool)
+   getgenv().startbabyclick = bool
+   print('Action is: ', bool)
+   if bool then
+       doclickbaby()
+   end
+end)
+
 x:Button("Collect Cash",function()
    while wait() do
     spawn(function()
@@ -75,6 +83,7 @@ for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
     end
 end)
 
+
 misc:DestroyGui()
 
 
@@ -96,6 +105,19 @@ function dopet()
 wait(.1)
 fireproximityprompt(petflop,1,true)
 wait(3)
+        end
+    end)
+end
+
+function doclickbaby()
+    spawn(function()
+        while getgenv().startbabyclick == true  do 
+            for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
+    if v.Name == 'ClickDetector' then
+        fireclickdetector(v)
+    end
+end
+wait()
         end
     end)
 end
