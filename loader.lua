5de8312de415abd889e23cda1acb98ed3b0352d3
LD2065 <89920318+LD0265@users.noreply.github.com>
2022-07-14 21:28:38 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,5 @@
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
 
-local notif = Notification.new("success", "Success", "Executing Script")
-    wait(2)
-    notif:delete()
-
 local chosenGame = ({
     [9660733037] = "peter.lua",
     [9312552871] = "floppa.lua",
@@ -20,10 +16,17 @@ if game.Players.LocalPlayer.DisplayName == 'elem_dee' then
         [1215581239] = "doomspireop.lua",
         [9921522947] = "floppa2op.lua"
     })[game.PlaceId]
-
+    
+    local notif = Notification.new("success", "Welcome ElEmDee", "Executing Owner Script")
+        wait(2)
+    notif:delete()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LD0265/sricpty-hub/main/" .. chosenGameop))()
 
 else
+    local notif = Notification.new("success", "Success", "Executing Script")
+    wait(2)
+    notif:delete()
+    
     loadstring(game:HttpGet("https://raw.githubusercontent.com/LD0265/sricpty-hub/main/" .. chosenGame))()
 
 end
