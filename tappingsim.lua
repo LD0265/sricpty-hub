7512eb4be4cfbb2fe54d5d7ebae581bf2e1e80ba
LD2065 <89920318+LD0265@users.noreply.github.com>
2022-07-16 15:01:43 -0400

    Update tappingsim.lua

--- tappingsim.lua
+++ tappingsim.lua
@@ -1,4 +1,2 @@
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
-
 local notif = Notification.new("error", "Error", "Script Whitelist Required - DM ElEmDee for Help")
-notif:deleteTimeout(3)
