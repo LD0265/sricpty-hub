87c5ec236dcb92a532bf83cb529644c5ecac7612
LD2065 <89920318+LD0265@users.noreply.github.com>
2022-07-15 22:47:08 -0400

    Update peter.lua

--- peter.lua
+++ peter.lua
@@ -5,7 +5,7 @@ local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloo
 local w = library:CreateWindow("ElEmDee Hub") -- Creates the window
 
 local b = w:CreateFolder("Main")
-
+local t = w:CreateFolder('Teleports')
 local c = w:CreateFolder("Misc")-- Creates the folder(U will put here your buttons,etc)
 
 
@@ -38,21 +38,12 @@ end
 end
 end)
 
-b:Button("Tp to Store",function()
+t:Button("Tp to Store",function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Scenery.GasStation.GasStation.Part.CFrame
     end)
 
-b:Button("Tp to House",function()
+t:Button("Tp to House",function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Scenery.House.HouseModel.HouseFrame.Floor.CFrame
 end)      
-
-c:Button("Crash Server",function()
-        local crashserverthing = game.workspace:GetDescendants()
-        
-        for i,v in pairs(crashserverthing) do
-            print(v)
-            end
-        end)
-
 
 c:DestroyGui()
