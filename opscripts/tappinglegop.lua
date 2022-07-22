439ac0b69a7e43aff52fdbbda3302f5ef8d12110
LD2065 <89920318+LD0265@users.noreply.github.com>
2022-07-21 21:52:22 -0400

    Update tappinglegop.lua

--- opscripts/tappinglegop.lua
+++ opscripts/tappinglegop.lua
@@ -1,3 +1,5 @@
+if loaded then
+
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
 
 local w = library:CreateWindow("ElEmDee Hub") -- Creates the window
@@ -75,3 +77,6 @@ wait()
         end
     end)
 end
+   else
+      print('Failed')
+   end
